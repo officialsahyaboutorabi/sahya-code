ab802a02afd3c64bf71aaed8c84489b1f680d0fc
Sahya Boutorabi <sahyaboutorabi@Sahyas-MacBook-Pro.local>
2026-04-02 12:02:13 +1100

    Update Mark component to use ASCII art logo instead of SVG

--- packages/ui/src/components/logo.tsx
+++ packages/ui/src/components/logo.tsx
@@ -2,16 +2,35 @@ import { ComponentProps } from "solid-js"
 
 export const Mark = (props: { class?: string }) => {
   return (
-    <svg
+    <div
       data-component="logo-mark"
       classList={{ [props.class ?? ""]: !!props.class }}
-      viewBox="0 0 16 20"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
+      style={{
+        "font-family": "monospace",
+        "font-size": "4px",
+        "line-height": "1",
+        color: "var(--icon-strong-base)",
+        "white-space": "pre",
+        display: "inline-block",
+      }}
     >
-      <path data-slot="logo-logo-mark-shadow" d="M12 16H4V8H12V16Z" fill="var(--icon-weak-base)" />
-      <path data-slot="logo-logo-mark-o" d="M12 4H4V16H12V4ZM16 20H0V0H16V20Z" fill="var(--icon-strong-base)" />
-    </svg>
+{`  /$$$$$$  /$$   /$$ /$$     /$$
+ /$$__  $$| $$  | $$|  $$   /$$/
+| $$  \ $$| $$  | $$ \  $$ /$$/ 
+| $$$$$$$$| $$$$$$$$  \  $$$$/  
+| $$__  $$| $$__  $$   \  $$/   
+| $$  | $$| $$  | $$    | $$    
+| $$  | $$| $$  | $$    | $$    
+|__/  |__/|__/  |__/    |__/    
+  /$$$$$$   /$$$$$$  /$$$$$$$  /$$$$$$$$
+ /$$__  $$ /$$__  $$| $$__  $$| $$_____/ 
+| $$  \__/| $$  \ $$| $$  \ $$| $$       
+| $$      | $$  | $$| $$  | $$| $$$$$    
+| $$      | $$  | $$| $$  | $$| $$__/    
+| $$    $$| $$  | $$| $$  | $$| $$       
+|  $$$$$$/|  $$$$$$/| $$$$$$$/| $$$$$$$$ 
+ \______/  \______/ |_______/ |________/`}
+    </div>
   )
 }
 
